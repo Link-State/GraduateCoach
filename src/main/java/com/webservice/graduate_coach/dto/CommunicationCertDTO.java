d0295afb42191c78d4fda7e1630ccdd706aa673b
LinkState <jjyyhh99@naver.com>
2025-05-23 11:30:58 +0900

    DB수정

--- src/main/java/com/webservice/graduate_coach/dto/CommunicationCertDTO.java
+++ src/main/java/com/webservice/graduate_coach/dto/CommunicationCertDTO.java
@@ -11,7 +11,8 @@ import lombok.*;
 public class CommunicationCertDTO {
     private Integer UID;
     private String name;
-    private Integer score;
+    private String descript;
+    private Float score;
     private Integer department;
     private Integer year;
 }
--- src/main/java/com/webservice/graduate_coach/entity/ForeignCertEntity.java
+++ src/main/java/com/webservice/graduate_coach/entity/ForeignCertEntity.java
@@ -18,7 +18,8 @@ public class ForeignCertEntity {
     private Integer UID;
 
     private String name;
-    private Integer score;
+    private String descript;
+    private Float score;
     private Integer department;
     private Integer year;
 }
