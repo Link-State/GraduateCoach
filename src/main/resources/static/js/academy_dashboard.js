84a32294b1cad4346ef245465cd7e11b467656ea
4Dalpaka <alpaka369@gmail.com>
2025-05-27 12:11:41 +0900

    학사관리 수정

--- src/main/resources/static/js/academy_dashboard.js
+++ src/main/resources/static/js/academy_dashboard.js
@@ -1,79 +1,69 @@
-// modla1 control
-const modal1 = document.querySelector('#modal1');
-const btnOpenModal1 = document.querySelector('#btn-opn-mod1');
-const btnCloseModal1 = document.querySelector('#btn-clo-mod1');
-btnOpenModal1.addEventListener("click", ()=>{
-    modal1.style.display="flex";
-    document.body.classList.add('modal-open');
+document.addEventListener("click", function (e) {
+    const target = e.target;
 
-    // const department = document.getElementById("department").value;
-    // const year = document.getElementById("year").value;
-    // const major = document.getElementById("major").value;
+    // mod1
+    if (target.matches("#btn-opn-mod1")) {
+        document.querySelector("#modal1").style.display = "flex";
+        document.body.classList.add('modal-open');
+        // const department = document.getElementById("department").value;
+            // const year = document.getElementById("year").value;
+            // const major = document.getElementById("major").value;
 
-    // const formData = new URLSearchParams();
-    // formData.append("department", department);
-    // formData.append("year", year);
-    // formData.append("major", major);
+            // const formData = new URLSearchParams();
+            // formData.append("department", department);
+            // formData.append("year", year);
+            // formData.append("major", major);
 
-    // fetch("/academy_dashboard/edit/pilgyo", {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //     body: formData.toString()
-    //   })
-    //     .then(res => res.text())
-    //     .then(html => {
-    //       tableBody.innerHTML = html;
-    //       bindCheckboxEvents();
-    //     })
-    //     .catch(err => {
-    //       console.error("불러오기 실패:", err);
-    //     });
-    // }
-});
-btnCloseModal1.addEventListener("click", ()=>{
-    modal1.style.display="none"
-    document.body.classList.remove('modal-open');
-});
+            // fetch("/academy_dashboard/edit/pilgyo", {
+            //     method: "POST",
+            //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            //     body: formData.toString()
+            //   })
+            //     .then(res => res.text())
+            //     .then(html => {
+            //       tableBody.innerHTML = html;
+            //       bindCheckboxEvents();
+            //     })
+            //     .catch(err => {
+            //       console.error("불러오기 실패:", err);
+            //     });
+            // }
+    }
+    if (target.matches("#btn-clo-mod1")) {
+        document.querySelector("#modal1").style.display = "none";
+        document.body.classList.remove('modal-open');
+    }
 
-// modal5 control
-const modal5 = document.querySelector('#modal5');
-const btnOpenModal5 = document.querySelector('#btn-opn-mod5');
-const btnCloseModal5 = document.querySelector('#btn-clo-mod5');
-btnOpenModal5.addEventListener("click", ()=>{
-    modal5.style.display="flex";
-    document.body.classList.add('modal-open');
-});
-btnCloseModal5.addEventListener("click", ()=>{
-    modal5.style.display="none"
-    document.body.classList.remove('modal-open');
-});
+    // mod5
+    if (target.matches("#btn-opn-mod5")) {
+        document.querySelector("#modal5").style.display = "flex";
+        document.body.classList.add('modal-open');
+    }
+    if (target.matches("#btn-clo-mod5")) {
+        document.querySelector("#modal5").style.display = "none";
+        document.body.classList.remove('modal-open');
+    }
 
-// modal6 control
-const modal6 = document.querySelector('#modal6');
-const btnOpenModal6 = document.querySelector('#btn-opn-mod6');
-const btnCloseModal6 = document.querySelector('#btn-clo-mod6');
-btnOpenModal6.addEventListener("click", ()=>{
-    modal6.style.display="flex";
-    document.body.classList.add('modal-open');
-});
-btnCloseModal6.addEventListener("click", ()=>{
-    modal6.style.display="none"
-    document.body.classList.remove('modal-open');
-});
+    // mod6
+    if (target.matches("#btn-opn-mod6")) {
+        document.querySelector("#modal6").style.display = "flex";
+        document.body.classList.add('modal-open');
+    }
+    if (target.matches("#btn-clo-mod6")) {
+        document.querySelector("#modal6").style.display = "none";
+        document.body.classList.remove('modal-open');
+    }
 
-// modal7 control
-const modal7 = document.querySelector('#modal7');
-const btnOpenModal7 = document.querySelector('#btn-opn-mod7');
-const btnCloseModal7 = document.querySelector('#btn-clo-mod7');
-btnOpenModal7.addEventListener("click", ()=>{
-    modal7.style.display="flex";
-    document.body.classList.add('modal-open');
+    // mod7
+    if (target.matches("#btn-opn-mod7")) {
+        document.querySelector("#modal7").style.display = "flex";
+        document.body.classList.add('modal-open');
+    }
+    if (target.matches("#btn-clo-mod7")) {
+        document.querySelector("#modal7").style.display = "none";
+        document.body.classList.remove('modal-open');
+    }
 });
-btnCloseModal7.addEventListener("click", ()=>{
-    modal7.style.display="none"
-    document.body.classList.remove('modal-open');
-});
-
 
 // main_dashboard_search
 document.getElementById("search_dashboard").addEventListener("submit", function(e) {
