2abe5cd8eeddadb088723602b58e3a69e9faf82c
4Dalpaka <alpaka369@gmail.com>
2025-05-28 00:04:16 +0900

    학사관리 수정, (교양 기초 edit, 이수 학점 edit, 자격증 edit)

--- src/main/resources/static/js/academy_dashboard.js
+++ src/main/resources/static/js/academy_dashboard.js
@@ -5,33 +5,11 @@ document.addEventListener("click", function (e) {
     if (target.matches("#btn-opn-mod1")) {
         document.querySelector("#modal1").style.display = "flex";
         document.body.classList.add('modal-open');
-        // const department = document.getElementById("department").value;
-            // const year = document.getElementById("year").value;
-            // const major = document.getElementById("major").value;
-
-            // const formData = new URLSearchParams();
-            // formData.append("department", department);
-            // formData.append("year", year);
-            // formData.append("major", major);
-
-            // fetch("/academy_dashboard/edit/pilgyo", {
-            //     method: "POST",
-            //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            //     body: formData.toString()
-            //   })
-            //     .then(res => res.text())
-            //     .then(html => {
-            //       tableBody.innerHTML = html;
-            //       bindCheckboxEvents();
-            //     })
-            //     .catch(err => {
-            //       console.error("불러오기 실패:", err);
-            //     });
-            // }
     }
     if (target.matches("#btn-clo-mod1")) {
         document.querySelector("#modal1").style.display = "none";
         document.body.classList.remove('modal-open');
+        closeModal();
     }
 
     // mod5
@@ -42,6 +20,7 @@ document.addEventListener("click", function (e) {
     if (target.matches("#btn-clo-mod5")) {
         document.querySelector("#modal5").style.display = "none";
         document.body.classList.remove('modal-open');
+        closeModal();
     }
 
     // mod6
@@ -52,6 +31,7 @@ document.addEventListener("click", function (e) {
     if (target.matches("#btn-clo-mod6")) {
         document.querySelector("#modal6").style.display = "none";
         document.body.classList.remove('modal-open');
+        closeModal();
     }
 
     // mod7
@@ -62,6 +42,79 @@ document.addEventListener("click", function (e) {
     if (target.matches("#btn-clo-mod7")) {
         document.querySelector("#modal7").style.display = "none";
         document.body.classList.remove('modal-open');
+        closeModal();
+    }
+
+    // delete pilgyo
+    if (e.target.id === "delete_pilgyo") {
+        const row = e.target.closest("tr");
+        const uid = row.dataset.uid;
+
+        const year = document.getElementById("year").value;
+        const department = document.getElementById("department").value;
+        const major = document.getElementById("major").value;
+
+        const formData = new URLSearchParams();
+        formData.append("type", 1);
+        formData.append("year", year);
+        formData.append("department", department);
+        formData.append("major", major);
+        formData.append("courses", uid);
+
+        fetch("/exclude-course", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: formData.toString()
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                return response.text();
+            }
+        })
+        .then(html => {
+            if (html) {
+                document.getElementById("mod_edit_pilgyo").innerHTML = html;
+            }
+        })
+        .catch(err => console.error("삭제 실패:", err));
+    }
+
+    // delete comm
+    if (e.target.id === "delete_comm") {
+        const row = e.target.closest("tr");
+        const uid = row.dataset.uid;
+
+        const year = document.getElementById("year").value;
+        const department = document.getElementById("department").value;
+        const major = document.getElementById("major").value;
+
+        const formData = new URLSearchParams();
+        formData.append("year", year);
+        formData.append("department", department);
+        formData.append("major", major);
+        formData.append("type", 2);
+        formData.append("certs", uid);
+
+        fetch("/delete-cert", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: formData.toString()
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                return response.text();
+            }
+        })
+        .then(html => {
+            if (html) {
+                document.getElementById("mod_edit_comm").innerHTML = html;
+            }
+        })
+        .catch(err => console.error("삭제 실패:", err));
     }
 });
 
@@ -93,14 +146,147 @@ document.getElementById("search_dashboard").addEventListener("submit", function(
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: formData.toString()
     })
-    .then(response => response.text())
+    .then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;
+        } else {
+            return response.text();
+        }
+    })
     .then(html => {
-        document.getElementById("show_result").innerHTML = html;
+        if (html) {
+            document.getElementById("show_result").innerHTML = html;
+        }
     })
     .catch(err => {
         console.error("!!! :", err);
     });
 });
+
+// edit credit
+document.getElementById("edit_credit").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const year = document.getElementById("year").value;
+    const department = document.getElementById("department").value;
+    const major = document.getElementById("major").value;
+
+    const formData = new URLSearchParams();
+    formData.append("year", year);
+    formData.append("department", department);
+    formData.append("major", major);
+
+    formData.append("pilgyo_credit", document.getElementById("pilgyo_cr_edit").value);
+    formData.append("daegyo_credit", document.getElementById("daegyo_cr_edit").value);
+    formData.append("jeontam_credit", document.getElementById("jeontam_cr_edit").value);
+    formData.append("advanced_credit", document.getElementById("advanced_cr_edit").value);
+    formData.append("jeonpil_credit", document.getElementById("jeonpil_cr_edit").value);
+    formData.append("jeonseon_credit", document.getElementById("jeonseon_cr_edit").value);
+
+    fetch("/edit-credit", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: formData.toString()
+    })
+    .then(response => {
+        if (response.redirected) {
+            window.location.href = response.url;
+        } else {
+            return response.text();
+        }
+    })
+    .then(html => {
+        if (html) {
+            document.getElementById("mod_edit_credit").innerHTML = html;
+        }
+    })
+    .catch(err => {
+        console.error("수정 실패:", err);
+    });
+});
+
+// add comm
+document.getElementById("insert_comm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const requiredFields = this.querySelectorAll("input[required]");
+    let hasEmpty = false;
+
+    for (const field of requiredFields) {
+        if (!field.value.trim()) {
+            field.classList.add("input-warning");
+            hasEmpty = true;
+        } else {
+            field.classList.remove("input-warning");
+        }
+    }
+
+    if (hasEmpty) return;
+
+    const year = document.getElementById("year").value;
+    const department = document.getElementById("department").value;
+    const major = document.getElementById("major").value;
+
+    const formData = new URLSearchParams();
+    formData.append("year", year);
+    formData.append("department", department);
+    formData.append("major", major);
+
+    formData.append("name", document.getElementById("comm_name").value);
+    formData.append("descript", document.getElementById("comm_descript").value);
+    formData.append("score", document.getElementById("comm_score").value);
+    formData.append("cert_type", 2);
+
+    fetch("/add-cert", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: formData.toString()
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                return response.text();
+            }
+        })
+        .then(html => {
+            if (html) {
+                document.getElementById("mod_edit_comm").innerHTML = html;
+            }
+        })
+        .catch(err => {
+            console.error("수정 실패:", err);
+        });
+});
+
+// exit modal and refresh
+function closeModal(){
+    const formData = new URLSearchParams();
+        formData.append("department", document.getElementById("department").value);
+        formData.append("year", document.getElementById("year").value);
+        formData.append("major", document.getElementById("major").value);
+
+        fetch("/academy_dashboard", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: formData.toString()
+        })
+        .then(response => {
+            if (response.redirected) {
+                window.location.href = response.url;
+            } else {
+                return response.text();
+            }
+        })
+        .then(html => {
+            if (html) {
+                document.getElementById("show_result").innerHTML = html;
+            }
+        })
+        .catch(err => {
+            console.error("!!! :", err);
+        });
+}
 
 // ??
 function showResult(){
